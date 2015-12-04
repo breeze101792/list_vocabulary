898ed79f7f3841b48ff8ee0dac1ffb5f61cb1e29
Shaowu <breeze101792@gmail.com>
2015-12-04 21:43:25 +0800

    implement do_sentence_list function

--- main.py
+++ main.py
@@ -2,17 +2,49 @@
 from optparse import OptionParser
 
 import settings
-class Data:
-    def __init__(self, file):
-        self.file = file
-    def make_sentence_list(self):
-        pass
+def printfun(it, s = '', e = '\n'):
+    for i in it:
+        print(s + i + e)
 
+class Data:
+    def __init__(self, file = None):
+        self.file = file
+        self.sentence_list = file
+        self.word_list = None
+    def do_sentence_list(self):
+        tmp_list = self.file.replace('\n', ' ').replace('  ', ' ').splitlines()
+        tmp = []
+        mark_list = ['.','!', '?', ':', '\"', "-", "*"]
+        for mark in mark_list:
+            tmp = []
+            for idx, line in enumerate(tmp_list):
+                if mark in line:
+                    tmp_sen = line.split(mark)
+                    if len(tmp_sen) > 1:
+                        tmp.extend(tmp_sen)
+                    else:
+                        tmp.append(tmp_sen)
+                else:
+                    tmp.append(line)
+            tmp_list = tmp
+        self.sentence_list = tmp_list
+
+        # for i in tmp:
+        #     print(i.lstrip())
+
+
+    def do_list(self):
+        self.sentence_list = self.file.splitlines()
+        for line in self.sentence_list:
+            print(line + "\n")
+        pass
+class Word:
+    pass
+
+# return word
 class ydict:
     pass
 
-class Word:
-    pass
 class Dictionary:
     pass
 
@@ -34,9 +66,14 @@ def main():
         settings.file_name = options.file_name
 
     # open file
-    with open(settings.file_name) as f:
-        for line in f:
-            print(line)
+    # with open(settings.file_name) as f:
+    #     for line in f:
+    #         print(line)
+    f = open(settings.file_name)
+    text = f.read()
+    f.close()
+    data = Data(text)
+    data.do_sentence_list()
 
     print("list all vocabulary")
     print("file name\t", settings.file_name)

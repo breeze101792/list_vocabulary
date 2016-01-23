0bb5ee726759904e78543cd66c83022519da26e1
Shaowu <breeze101792@gmail.com>
2016-01-23 22:44:32 +0800

    1.add exception handle, OSError and EOFError 2.use setting.py to store program msg

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ from optparse import OptionParser
 from data import Data
 from dictionary import Word
 from ydict import ydict
+from settings import Settings
 
 
 import settings
-def printfun(it, s = '', e = '\n'):
+
+
+def printfun(it, s='', e='\n'):
     for i in it:
         print(s + i + e)
 
 
 def main():
-    parser = OptionParser(usage = 'Usage: login ......')
-    parser.add_option("-f", "--file", dest="file_name", help="file that you would like to parse", action="store")
-    parser.add_option("-w", "--word", dest="word", help="word that you would like to get its meaning", action="store")
-    parser.add_option("-t", "--test", dest="test", help="testing function", action="store")
-
+    parser = OptionParser(usage='Usage: login ......')
+    parser.add_option("-f", "--file", dest="file_name",
+                      help="file that you would like to parse", action="store")
+    parser.add_option("-w", "--word", dest="word",
+                      help="word that you would like to get its meaning", action="store")
+    parser.add_option("-t", "--test", dest="test",
+                      help="testing function", action="store")
 
     # parser.add_option("-p", "--password", dest="user_password", help="User password for log in", action="store")
     # parser.add_option("-s", "--serial-number", dest="serial_number", help="Project's serial number", action="store")
@@ -26,30 +31,56 @@ def main():
     # parser.add_option("-i", "--sign-in", dest="sign_in", help="Sign in",default=False, action="store_true")
     # parser.add_option("-o", "--sign-out", dest="sign_out", help="Sign out",default=False, action="store_true")
     (options, args) = parser.parse_args()
+    psettings = Settings()
 
-    #set up settings.py!
+    # set up settings.py!
+    # mode settings
+    if options.word is not None:
+        psettings.set('mode', 'word')
+        psettings.set('word', options.word)
     if options.file_name is not None:
-        settings.file_name = options.file_name
-    else:
-        settings.file_name = "data/i have a dream.txt"
+        psettings.set('mode', 'file')
+        psettings.set('file_name', options.file_name)
+    if options.file_name is not None:
+        psettings.set('mode', 'test')
+        psettings.set('file_name', options.file_name)
 
     # print("list all vocabulary")
     # print("file name\t", settings.file_name)
-
+    my_dict = ydict()
     # open file
-    if options.file_name is not None:
-        f = open(settings.file_name)
-        text = f.read()
-        f.close()
-        data = Data(text)
-        data.do_word_list()
-        data.word_report()
-    if options.word is not None:
-        # print("search single word!")
-        test = ydict()
-        test.search(options.word)
-    if options.test is not None:
-        word = Word()
-        word.show_meaning()
+    try:
+        if psettings.get('mode') == 'interactive':
+            try:
+                while True:
+                    word = input(psettings.get('name').__str__() + ">")
+                    my_dict.search(word)
+            except EOFError:
+                print(psettings.get('msg_exit'))
+            except:
+                raise
+        elif psettings.get('mode') == 'word':
+            # print("search single word!")
+            my_dict.search(options.word)
+        elif psettings.get('mode') == 'file':
+            f = open(settings.file_name)
+            text = f.read()
+            f.close()
+            data = Data(text)
+            data.do_word_list()
+
+            for w in data.get_word_list():
+                my_dict.search(w)
+        elif psettings.get('mode') == 'test':
+            f = open(settings.file_name)
+            text = f.read()
+            f.close()
+            data = Data(text)
+            data.do_word_list()
+            data.word_report()
+    except OSError:
+        print(psettings.get('msg_err_connection'))
+    except:
+        raise
 if __name__ == '__main__':
     main()
--- settings.py
+++ settings.py
@@ -10,16 +10,14 @@ class Settings:
     'dictionary':'ydict',\
     \
     'word':'test',\
+    \
+    'msg_exit':'Have a nice day!',\
+    'msg_err_connection':'please check out you internet connection!',\
     }
     def set(self, name, value):
         self.__setting_dict[name] = value
     def get(self, name):
         return self.__setting_dict[name]
-
-working_dirtory = "./"
-file_name = ""
-dictionary = ""
-
 
 # test = setting()
 # test.set('file_name', 'test')

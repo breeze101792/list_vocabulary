e655370328ea13a75e2aa03e044c2f324a1f8602
Shaowu <breeze101792@gmail.com>
2016-01-24 21:05:27 +0800

    1.add insert function in wordbank 2.update word with arguments

--- wordbank.py
+++ wordbank.py
@@ -25,7 +25,7 @@ class WordBank:
             # We can also close the connection if we are done with it.
             # Just be sure any changes have been committed or they will be lost.
             conn.close()
-            print('successful init')
+            # print('successful init')
     def __lock(self):
         if self.__db_lock == False:
             self.__db_lock = True
@@ -42,7 +42,7 @@ class WordBank:
         return self.__db_lock
     def connect(self):
         if self.__is_locked():
-            print("Database is locked!")
+            # print("Database is locked!")
             return False
         elif self.__db_connection is None:
             self.__lock()
@@ -51,56 +51,75 @@ class WordBank:
             self.__unlock()
             return True
         else:
-            print("Database is already connected!")
+            # print("Database is already connected!")
             return True
     def close(self):
         if self.__db_lock:
-            print('database is locked!\n please unlocked first!')
+            # print('database is locked!\n please unlocked first!')
             return False
         elif self.__db_connection is None:
-            print('there is nothing to do!')
+            # print('there is nothing to do!')
+            return True
         else:
             self.commit()
             self.__db_connection.close()
+            return True
     def commit(self):
         if self.__is_locked():
-            print('db is locked')
+            # print('db is locked')
             return False
         else:
             self.__db_connection.commit()
             return True
     def quer_for_all_word(self):
         if self.__is_locked():
-            print('db is locked')
+            # print('db is locked')
             return False
         else:
             self.__lock()
             query_str = """SELECT * FROM WORD;"""
             result = self.__cursor.execute(query_str)
             self.__unlock()
-            return result.fetchone()
-    def update(self, word):
-        query_str = "SELECT times FROM WORD WHERE word == '%s'" % word
+            return result.fetchall()
+    def update(self, word, times = 0, familiar = 0):
+        query_str = "SELECT times, familiar FROM WORD WHERE word == '%s'" % word
 
         if self.__is_locked():
-            print('db is locked')
+            # print('db is locked')
             return False
         else:
             self.__lock()
-            result = self.__cursor.execute(query_str).fetchone()[0]
-
+            result = self.__cursor.execute(query_str).fetchone()
             if result is None:
                 self.__unlock()
                 return False
             else:
-                query_str = "UPDATE WORD SET times = %d WHERE word == '%s'" % (result + 1, word)
+                query_str = "UPDATE WORD SET times = %i, familiar = %i WHERE word == '%s'" % (result[0] + times, result[1] + familiar, word)
                 result = self.__cursor.execute(query_str).fetchone()
             self.__unlock()
             return result #.fetchone()
+    def insert(self, word, familiar = 0):
+        query_str = "SELECT word FROM WORD WHERE word == '%s'" % word
 
+        if self.__is_locked():
+            # print('db is locked')
+            return False
+        else:
+            self.__lock()
+            result = self.__cursor.execute(query_str).fetchone()
+            if result is not None:
+                # word is already in the wordbank
+                self.__unlock()
+                return False
+            else:
+                query_str = "INSERT INTO WORD (word, times, familiar) VALUES ('%s', %i, %i)" % (word, 1, familiar)
+                result = self.__cursor.execute(query_str).fetchone()
+            self.__unlock()
+            return result #.fetchone()
 test = WordBank()
 test.db__init()
 test.connect()
-print(test.update('a'))
+test.insert('test')
+print(test.update('test', 1))
 print(test.quer_for_all_word())
 test.close()

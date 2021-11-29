7f9d101d15174eff6cb8139a2e74ddcce9443ef3
Shaun Tseng <breeze101792@gmail.com>
2021-11-29 17:36:34 +0000

    [script] Refine setup file
    [Description]
    Refine setup file
    
    [Self-Test]
    N/A

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 function dict_init()
 {
     local cpath=`pwd`
@@ -8,7 +9,7 @@ function dict_init()
     {
         7z x $dict_file
         iconv -f utf8 -t gb2312 stardict.csv | iconv -f gb2312 -t big5 | iconv -f big5 -t utf8 > stardict_t.csv
-        python $cpath/ecdict.py dict.db stardict_t.csv
+        python3 $cpath/ecdict.py dict.db stardict_t.csv
         cp dict.db $cpath
     }
     popd
@@ -21,8 +22,37 @@ function generate_exc()
     echo "python3 $cpath/main.py \$@" >> $exc_file
     chmod u+x $exc_file
 }
-if [ ! -f dict.db ]
-then
-    dict_init
-fi
-generate_exc
+
+function fHelp()
+{
+    echo "Setup Usage"
+    printf "    %s%s%s\n" "-d|--dict" "->" "Setup dict db"
+    printf "    %s%s%s\n" "-h|--help" "->" "Help me"
+    echo "Note."
+    echo "      Step 1. Do submodule init/update"
+    echo "      Step 2. Install p7zip-full, iconv, pyton3."
+}
+
+function setup()
+{
+    while [ "$#" != "0" ]
+    do
+        case $1 in
+            -d|--dict)
+                dict_init
+                generate_exc
+                ;;
+            -h|--help)
+                fHelp
+                return 0
+                ;;
+            *)
+                echo "Unknown Args"
+                fHelp
+                return 1
+                ;;
+        esac
+        shift 1
+    done
+}
+setup $@
